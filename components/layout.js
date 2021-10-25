4ce13b9466ae62232f4c63b9431c0df35f47fad6
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2021-10-25 16:32:11 +0900

    layout컴포넌트에 CSS모듈로 스타일하기

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,9 @@
+// export default function Layout({ children }) {
+//   return <div>{children}</div>;
+// }
+
+import styles from "./layout.module.css";
+
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 }
